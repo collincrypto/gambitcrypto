989911fa98050fad2d57a2758699ed2438d78a4d
collinlahay <raithe@gmail.com>
2015-04-13 19:15:15 -0500

    Gambit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Gambit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GambitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
